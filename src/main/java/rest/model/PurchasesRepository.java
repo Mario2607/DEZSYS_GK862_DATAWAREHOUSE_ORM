cd4d23e1f17100a070830fcfd2602a72a5ec9dcd
Mario2607 <mmilkov@student.tgm.ac.at>
2026-03-24 15:16:29 +0100

    kleine sache

--- src/main/java/rest/model/PurchasesRepository.java
+++ src/main/java/rest/model/PurchasesRepository.java
@@ -1,6 +1,8 @@
 package rest.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PurchasesRepository extends CrudRepository<PurchasesData, Long> {
 }
